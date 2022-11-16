ebde64193f5dc8ef0c42177f6c7a35873352fc57
Anastasiia Solovei <av.ignatyuk@gmail.com>
2022-11-16 17:49:30 +0200

    add general reset styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'node_modules/modern-normalize/modern-normalize.css';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
 
