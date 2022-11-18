858bc103c9ef46dbde118d895142ea7c6d1c0992
Anastasiia Solovei <av.ignatyuk@gmail.com>
2022-11-18 16:27:37 +0200

    refactoring of currency rate component

--- src/components/CurrencyRate/CurrencyRate.js
+++ src/components/CurrencyRate/CurrencyRate.js
@@ -1,32 +1,52 @@
 import { useEffect, useState } from 'react';
 import { currencies } from '../../assets/constants';
 import currencyApi from '../../services/currencyApi';
+import CurrencyItem from '../CurrencyItem';
 
-import sprite from '../../images/svg_sprite.svg';
 import styles from './CurrencyRate.module.css';
 
 export default function CurrencyRate() {
   const [currency, setCurrency] = useState([]);
 
   useEffect(() => {
-    currencyApi
-      .fetchCurrency()
-      .then(data => console.log(data))
-      .catch(error => console.log(error));
+    const fetchCurrency = async () => {
+      const data = await currencyApi.fetchCurrency();
+      const filtredCurrencies = [];
+
+      currencies.forEach(currency => {
+        data.forEach(element => {
+          if (element.ccy === currency) {
+            filtredCurrencies.push({
+              ccy: element.ccy,
+              buy: Number(element.buy).toFixed(2),
+              sale: Number(element.sale).toFixed(2),
+            });
+          }
+        });
+      });
+
+      setCurrency(filtredCurrencies);
+      localStorage.setItem('currency', JSON.stringify(filtredCurrencies));
+      localStorage.setItem('currencyTime', Date.now());
+    };
+
+    let currencyLS = JSON.parse(localStorage.getItem('currency'));
+    let currencyTime = JSON.parse(localStorage.getItem('currencyTime'));
+    if (!currencyLS) {
+      fetchCurrency();
+    }
+    if (Date.now() - currencyTime > 3600000) {
+      fetchCurrency();
+    } else {
+      setCurrency(currencyLS);
+    }
   }, []);
 
   return (
-    <ul className={styles.CurrencyRate}>
-      <li>
-        <svg width="20px" height="20px">
-          <use href={sprite + '#icon-dollar'}></use>
-        </svg>
-      </li>
-      <li>
-        <svg width="20px" height="20px">
-          <use href={sprite + '#icon-eur'}></use>
-        </svg>
-      </li>
+    <ul className={styles.CurrencyList}>
+      {currency.map(({ ccy, buy, sale }) => {
+        return <CurrencyItem key={ccy} buy={buy} sale={sale} />;
+      })}
     </ul>
   );
 }
