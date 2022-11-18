5e658dad216fbc5e6a5560b63774adb0e1013935
Anastasiia Solovei <av.ignatyuk@gmail.com>
2022-11-18 16:28:05 +0200

    add currency item component

---
+++ src/components/CurrencyItem/CurrencyItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import sprite from '../../images/svg_sprite.svg';
+import styles from './CurrencyItem.module.css';
+
+export default function CurrencyItem({ ccy, buy, sale }) {
+  return (
+    <li key={ccy} className={styles.CurrencyItem}>
+      <svg width="20px" height="20px">
+        <use
+          href={ccy === 'USD' ? sprite + '#icon-dollar' : sprite + '#icon-eur'}
+        ></use>
+      </svg>
+      <span className={styles.CurrencyElement}>{buy}</span>
+      <span className={styles.CurrencyElement}>/</span>
+      <span className={styles.CurrencyElement}>{sale}</span>
+    </li>
+  );
+}
