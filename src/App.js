8a4e72d11e22910822a7dbc2def1dbcfd31aa229
Anastasiia Solovei <av.ignatyuk@gmail.com>
2022-11-22 18:15:54 +0200

    connect currency converter to root app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import CurrencyConverter from './components/CurrencyConverter';
 
 function App() {
   return (
     <>
       <Header />
+      <CurrencyConverter />
     </>
   );
 }
