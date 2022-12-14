57c841e31ac891ca9510da36a7dea89757318c9d
Anastasiia Solovei <av.ignatyuk@gmail.com>
2022-12-14 23:40:14 +0200

    connect footer component to root app file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import CurrencyConverter from './components/CurrencyConverter';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <CurrencyConverter />
+      <Footer />
     </>
   );
 }
